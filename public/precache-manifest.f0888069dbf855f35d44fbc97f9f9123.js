305249eed9418616f30d50c6398faa7fed1e0bde
Your Name <you@example.com>
2019-03-08 20:42:20 -0300

    Primer commit

---
+++ public/precache-manifest.f0888069dbf855f35d44fbc97f9f9123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "45489d95bebea48dcda8",
+    "url": "/static/css/main.6372792e.chunk.css"
+  },
+  {
+    "revision": "45489d95bebea48dcda8",
+    "url": "/static/js/main.45489d95.chunk.js"
+  },
+  {
+    "revision": "9b8e98f4de2327acc36e",
+    "url": "/static/js/1.9b8e98f4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8a6cdfac1acdbb3a8ef77423588164b2",
+    "url": "/index.html"
+  }
+];
